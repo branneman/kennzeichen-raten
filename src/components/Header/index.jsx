2533c4f625ceee896af380517ffa905c88211803
Bran van der Meer <bran.van.der.meer@protonmail.com>
2023-12-06 19:20:02 +0100

    revert(ca1089f): fix: Remove language switch
    This reverts commit ca1089f99d35f46ff1f4e3b0fb682becb970aeb9.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom'
+
+import { useTranslation } from '../../hooks/translation'
 import './index.css'
 
 export default function Header() {
+  const { t, language, setLanguage } = useTranslation()
+  const otherLanguage = language === 'de' ? 'en' : 'de'
+
   return (
     <header className="header">
       <h1>
         <Link to={`/`}>Kennzeichen-Raten</Link>
       </h1>
+      <p
+        className="header__switchlanguage"
+        onClick={() => setLanguage(otherLanguage)}
+      >
+        {t('components.Header.switch-language')}
+      </p>
     </header>
   )
 }
